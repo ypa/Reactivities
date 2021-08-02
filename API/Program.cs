6e8800120050288ddc0f86e1a19329a1286d6bc0
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-01 20:30:34 -0700

    Add Seed data script and run it on program start up

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
@@ -24,7 +24,8 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
             }
             catch (Exception ex)
             {
@@ -32,7 +33,7 @@ namespace API
                 logger.LogError(ex, "An error occured during migration");
             }
 
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
