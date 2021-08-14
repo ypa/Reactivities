957fd7845aec337f0bedefd84390dbce61af4655
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-13 20:27:53 -0700

    Add handling of deleteActivity

--- client-app/src/features/activities/dashoard/ActivityList.tsx
+++ client-app/src/features/activities/dashoard/ActivityList.tsx
@@ -5,9 +5,14 @@ import { Activity } from '../../../app/models/activity';
 interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({ activities, selectActivity }: Props) {
+export default function ActivityList({
+  activities,
+  selectActivity,
+  deleteActivity,
+}: Props) {
   return (
     <Segment>
       <Item.Group divided>
@@ -28,6 +33,12 @@ export default function ActivityList({ activities, selectActivity }: Props) {
                   floated="right"
                   content="View"
                   color="blue"
+                />
+                <Button
+                  onClick={() => deleteActivity(activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
