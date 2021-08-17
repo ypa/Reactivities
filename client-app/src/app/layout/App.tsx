5d5babe923f13ddcdf2ce17d19f091c519f7874c
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-16 20:28:25 -0700

    Setup MobX store and wire up with ActivityStore

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,8 +6,11 @@ import ActivityDashboard from '../../features/activities/dashoard/ActivityDashbo
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
@@ -81,6 +84,8 @@ function App() {
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
+        <h2>Testing mobx store: {activityStore.title}</h2>
+
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
