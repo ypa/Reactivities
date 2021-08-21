82f2f17dbd411110f21b2e168edcff2350223ade
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-20 20:13:13 -0700

    Create HomePage and wire with to routes with respective components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,9 @@ import ActivityDashboard from '../../features/activities/dashoard/ActivityDashbo
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 function App() {
   const { activityStore } = useStore();
@@ -20,7 +23,9 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </>
   );
