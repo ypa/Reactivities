03309e0fc01ad41e5f7d2d9468cd02a294cd8189
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-22 20:21:31 -0700

    Add location key to the router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,12 +3,14 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashoard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <NavBar />
@@ -17,6 +19,7 @@ function App() {
         <Route exact path="/activities" component={ActivityDashboard} />
         <Route path="/activities/:id" component={ActivityDetails} />
         <Route
+          key={location.key}
           path={['/createActivity', '/manage/:id']}
           component={ActivityForm}
         />
