809a1dd26c7de15e5a4d983bf7f8f43622746897
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-28 20:33:37 -0700

    Add AppException middleware to display error in JSON in case of unhandled exception

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ using Persistence;
 using Application.Core;
 using API.Extensions;
 using FluentValidation.AspNetCore;
+using API.Middleware;
 
 namespace API
 {
@@ -47,9 +48,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
