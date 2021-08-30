25b70d6ccb2da7710cc6ae5bd3df7c633c210ca9
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-29 20:41:48 -0700

    Add text to the logo

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -12,6 +12,7 @@ export default function NavBar() {
             alt="logo"
             style={{ marginRight: '10px' }}
           />
+          Reactivities
         </Menu.Item>
         <Menu.Item as={NavLink} to="/activities" name="Activities" />
         <Menu.Item as={NavLink} to="/errors" name="Errors" />
