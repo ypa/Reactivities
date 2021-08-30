c267a4199b0b16eac7702eea2ea3c6209209e566
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-29 20:17:03 -0700

    Import ToastContainer from react-toastify

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,12 +8,14 @@ import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route exact path="/" component={HomePage} />
       <Route
         path={'/(.+)'}
