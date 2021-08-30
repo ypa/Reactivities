c524a3ec91cb4e9a72115c2af55402eed74037e2
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-29 20:12:56 -0700

    Wire up error page for testing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,7 @@ import { Route, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
 
 function App() {
   const location = useLocation();
@@ -27,6 +28,7 @@ function App() {
                 path={['/createActivity', '/manage/:id']}
                 component={ActivityForm}
               />
+              <Route path="/errors" component={TestErrors} />
             </Container>
           </>
         )}
