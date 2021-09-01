65a9b06b74f2dd7baee6614c048e1a060eb52571
Yan P Aung <yan.pye.aung@gmail.com>
2021-08-31 20:36:34 -0700

    Refactor ActivityForm with Formik Form and Fields: WIP

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,10 +2,11 @@ import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useState } from 'react';
 import { useEffect } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
+import { Formik, Form, Field } from 'formik';
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -33,86 +34,63 @@ export default observer(function ActivityForm() {
     if (id) loadActivity(id).then((activity) => setActivity(activity!));
   }, [id, loadActivity]);
 
-  function handleSubmit() {
-    if (activity.id.length === 0) {
-      let newActivity = {
-        ...activity,
-        id: uuid(),
-      };
-      createActivity(newActivity).then(() =>
-        history.push(`/activities/${newActivity.id}`)
-      );
-    } else {
-      updateActivity(activity).then(() =>
-        history.push(`/activites/${activity.id}`)
-      );
-    }
-  }
+  // function handleSubmit() {
+  //   if (activity.id.length === 0) {
+  //     let newActivity = {
+  //       ...activity,
+  //       id: uuid(),
+  //     };
+  //     createActivity(newActivity).then(() =>
+  //       history.push(`/activities/${newActivity.id}`)
+  //     );
+  //   } else {
+  //     updateActivity(activity).then(() =>
+  //       history.push(`/activites/${activity.id}`)
+  //     );
+  //   }
+  // }
 
-  function handleInputChange(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { name, value } = event.target;
-    setActivity({ ...activity, [name]: value });
-  }
+  // function handleChange(
+  //   event: ChangeEventFieldElement | HTMLTextAreaElement>
+  // ) {
+  //   const { name, value } = event.target;
+  //   setActivity({ ...activity, [name]: value });
+  // }
 
   if (loadingInitial) return <LoadingComponent content="Loading activity..." />;
 
   return (
     <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete="off">
-        <Form.Input
-          placeholder="Title"
-          value={activity.title}
-          name="title"
-          onChange={handleInputChange}
-        />
-        <Form.TextArea
-          placeholder="Description"
-          value={activity.description}
-          name="description"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          placeholder="Category"
-          value={activity.category}
-          name="category"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          type="date"
-          placeholder="Date"
-          value={activity.date}
-          name="date"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          placeholder="City"
-          value={activity.city}
-          name="city"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          placeholder="Venue"
-          value={activity.venue}
-          name="venue"
-          onChange={handleInputChange}
-        />
-        <Button
-          loading={loading}
-          floated="right"
-          positive
-          type="submit"
-          content="Submit"
-        />
-        <Button
-          as={Link}
-          to="/activities"
-          floated="right"
-          type="button"
-          content="Cancel"
-        />
-      </Form>
+      <Formik
+        enableReinitialize
+        initialValues={activity}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleSubmit }) => (
+          <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+            <Field placeholder="Title" name="title" />
+            <Field placeholder="Description" name="description" />
+            <Field placeholder="Category" name="category" />
+            <Field type="date" placeholder="Date" name="date" />
+            <Field placeholder="City" name="city" />
+            <Field placeholder="Venue" name="venue" />
+            <Button
+              loading={loading}
+              floated="right"
+              positive
+              type="submit"
+              content="Submit"
+            />
+            <Button
+              as={Link}
+              to="/activities"
+              floated="right"
+              type="button"
+              content="Cancel"
+            />
+          </Form>
+        )}
+      </Formik>
     </Segment>
   );
 });
