09fdb218cba2fe3d23a44d86fc605123d3a64e17
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-06 21:15:22 -0700

    Add user model types and agent client methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import { Activity } from '../models/activity';
 import { history } from '../../index';
 import { store } from '../stores/store';
+import { User, UserFormValues } from '../models/user';
 
 
 const sleep = (delay: number) => {
@@ -70,8 +71,15 @@ const Activities = {
   delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+  current: () => request.get<User>('/account'),
+  login: (user: UserFormValues) => request.post<User>('/account/login', user),
+  register: (user: UserFormValues) => request.post<User>('/account/register', user)
+}
+
 const agent = {
-  Activities
+  Activities,
+  Account
 }
 
 export default agent;
