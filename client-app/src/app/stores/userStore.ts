f085a1795eb68a68a6cb0fc7e68be2067751cfa4
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-07 20:30:10 -0700

    Store user jwt token in localstorage

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { history } from "../..";
 import agent from "../api/agent";
 import { User, UserFormValues } from "../models/user";
+import { store } from "./store";
 
 export default class UserStore {
   user: User | null = null;
@@ -16,7 +18,9 @@ export default class UserStore {
   login = async (creds: UserFormValues) => {
     try {
       const user = await agent.Account.login(creds);
-      console.log(user);
+      store.commonStore.setToken(user.token);
+      runInAction(() => this.user = user);
+      history.push('/activities');
     } catch (error) {
       throw error;
     }
