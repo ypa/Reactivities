d7a66a17950ea482d10d3f0f4073a0529e2ebfbc
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-08 20:40:02 -0700

    Persist jwt token inside localstorage and send it with every request after logged in

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,13 +1,25 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
   error: ServerError | null = null;
-  token: string | null = null;
+  token: string | null = window.localStorage.getItem('jwt');
   appLoaded = false;
 
   constructor() {
     makeAutoObservable(this);
+
+    // This reaction is only called after token changes
+    reaction(
+      () => this.token,
+      token => {
+        if (token) {
+          window.localStorage.setItme('jwt', token)
+        } else {
+          window.localStorage.remoeItem('jwt')
+        }
+      }
+    )
   }
 
   setServerError = (error: ServerError) => {
