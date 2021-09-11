87719d600810c16610fe371622aa3d5722e80541
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-10 21:14:33 -0700

    Configure Many-to-many JoinTable ActivityAttendee with custom column IsHost

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Domain
 {
@@ -6,7 +7,7 @@ namespace Domain
     {
         public Guid Id { get; set; }
 
-        public string Title {get; set; }
+        public string Title { get; set; }
 
         public DateTime Date { get; set; }
 
@@ -17,5 +18,7 @@ namespace Domain
         public string City { get; set; }
 
         public string Venue { get; set; }
+
+        public ICollection<ActivityAttendee> Attendees { get; set; }
     }
 }
