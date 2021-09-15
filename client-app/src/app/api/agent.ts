65f5b7adbdbeb62401f5c651c96f40b71b7bc6cd
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-14 20:22:37 -0700

    Add store handler methods for users attending activity

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -73,9 +73,10 @@ const request = {
 const Activities = {
   list: () => request.get<Activity[]>('/activities'),
   details: (id: string) => request.get<Activity>(`/activities/${id}`),
-  create: (activity: Activity) => axios.post<void>('/activities', activity),
-  update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-  delete: (id: string) => axios.delete<void>(`/activities/${id}`)
+  create: (activity: Activity) => request.post<void>('/activities', activity),
+  update: (activity: Activity) => request.put<void>(`/activities/${activity.id}`, activity),
+  delete: (id: string) => request.del<void>(`/activities/${id}`),
+  attend: (id: string) => request.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
