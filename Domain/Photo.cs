fabed114b5677848c6331a98e87a181b1b065343
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-17 20:25:28 -0700

    Add Photo Database entities

---
+++ Domain/Photo.cs
@@ -0,0 +1,13 @@
+namespace Domain
+{
+    public class Photo
+    {
+
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsMain { get; set; }
+
+    }
+}
