90760cdd68caca6cd0dc1faa4992f5cf9d7b3a67
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-18 20:21:45 -0700

    Return AttendeeDto with image when listing activities

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -24,7 +24,7 @@ namespace Application.Activities
 
         public bool IsCancelled { get; set; }
 
-        public ICollection<Profile> Attendees { get; set; }
+        public ICollection<AttendeeDto> Attendees { get; set; }
 
     }
 }
