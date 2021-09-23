63b291c6e6e478fd78bb8438ddf205218ae891b5
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-22 20:37:44 -0700

    Add handleDeletePhoto functionality

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -17,6 +17,7 @@ export default observer(function ProfilePhotos({ profile }: Props) {
       uploading,
       loading,
       setMainPhoto,
+      deletePhoto,
     },
   } = useStore();
   const [addPhotoMode, setAddPhotoMode] = useState(false);
@@ -32,6 +33,14 @@ export default observer(function ProfilePhotos({ profile }: Props) {
   ) {
     setTarget(e.currentTarget.name);
     setMainPhoto(photo);
+  }
+
+  function handleDeletePhoto(
+    photo: Photo,
+    e: SyntheticEvent<HTMLButtonElement>
+  ) {
+    setTarget(e.currentTarget.name);
+    deletePhoto(photo);
   }
 
   return (
@@ -65,12 +74,20 @@ export default observer(function ProfilePhotos({ profile }: Props) {
                         basic
                         color="green"
                         content="Main"
+                        name={'main' + photo.id}
+                        disabled={photo.isMain}
+                        loading={target === 'main' + photo.id && loading}
+                        onClick={(e) => handleSetMainPhoto(photo, e)}
+                      />
+                      <Button
+                        basic
+                        color="red"
+                        icon="trash"
                         name={photo.id}
                         disabled={photo.isMain}
                         loading={target === photo.id && loading}
-                        onClick={(e) => handleSetMainPhoto(photo, e)}
+                        onClick={(e) => handleDeletePhoto(photo, e)}
                       />
-                      <Button basic color="red" icon="trash" />
                     </Button.Group>
                   )}
                 </Card>
