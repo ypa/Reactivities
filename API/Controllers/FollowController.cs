aec4e1698f1ccaf4c85ede22e7ff374ce3705095
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-27 20:28:03 -0700

    Add handler and controller for querying user's following and follower profiles

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Application.Followers;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +10,12 @@ namespace API.Controllers
         public async Task<IActionResult> Follow(string username)
         {
             return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+        }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
         }
     }
 }
