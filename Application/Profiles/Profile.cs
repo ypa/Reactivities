040ca9045d81089b590b40237053cf1787ae9fe6
Yan P Aung <yan.pye.aung@gmail.com>
2021-09-27 20:05:29 -0700

    Add follower count and following count to user profiles

--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -13,6 +13,13 @@ namespace Application.Profiles
 
         public string Image { get; set; }
 
+        public bool Following { get; set; }
+
+
+        public int FollowersCount { get; set; }
+
+        public int FollowingCount { get; set; }
+
         public ICollection<Photo> Photos { get; set; }
     }
 }
