cf7a00835354374d4bafc42e4cda864e6bed7e4e
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-06 20:36:21 -0700

    Confgure Content security policy for realy based on browser console errors generated by CspReportOnly config

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,14 +64,14 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCspReportOnly(opt => opt
+            app.UseCsp(opt => opt
                 .BlockAllMixedContent()
-                .StyleSources(s => s.Self())
-                .FontSources(s => s.Self())
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self())
-                .ScriptSources(s => s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ScriptSources(s => s.Self().CustomSources("sha256-Rh8MTEBtFMyFx6O4lsoOVvHhuLlcTgXK7ygQ7YPzl5A="))
             );
 
             if (env.IsDevelopment())
