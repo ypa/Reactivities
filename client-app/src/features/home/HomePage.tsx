15641b7f53c03388757cf99e25e4aab6152f417c
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-23 19:20:45 -0700

    Handle facebook login on the client side

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -56,6 +56,7 @@ export default observer(function HomePage() {
               Or
             </Divider>
             <Button
+              loading={userStore.fbLoading}
               size="huge"
               inverted
               color="facebook"
