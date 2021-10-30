23e18878a4ace27bb90c3169d1f1766412cee999
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-30 14:28:45 -0700

    update getUser to set user token in store

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -41,6 +41,7 @@ export default class UserStore {
   getUser = async () => {
     try {
       const user = await agent.Account.current();
+      store.commonStore.setToken(user.token);
       runInAction(() => this.user = user);
       this.startRefreshTokenTimer(user);
     } catch (error) {
