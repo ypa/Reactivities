ac9700c35838880edd871a2e6f0a60e07712e347
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-30 14:16:28 -0700

    Set pagination header at policy level instead to fix activities no longer loading issue

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -69,7 +69,7 @@ namespace API.Extensions
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials()
-                        .WithExposedHeaders("WWW-Authenticate")
+                        .WithExposedHeaders("WWW-Authenticate", "Pagination")
                         .WithOrigins("http://localhost:3000");
                 });
             });
