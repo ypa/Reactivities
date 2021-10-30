c4c358b27f53ec4db71276a1cddc61628c9ae9c0
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-30 14:10:10 -0700

    Add refresh token timer in the client and ask user to re-login when token expires

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -35,7 +35,7 @@ axios.interceptors.response.use(async response => {
 
   return response;
 }, (error: AxiosError) => {
-  const { data, status, config } = error.response!;
+  const { data, status, config, headers } = error.response!;
   switch (status) {
     case 400:
       if (typeof data === 'string') {
@@ -55,7 +55,10 @@ axios.interceptors.response.use(async response => {
       }
       break;
     case 401:
-      toast.error('unauthorised');
+      if (status === 401 && headers['www-authenticate'].startsWith('Bearer error="invalid_token"')) {
+        store.userStore.logout();
+        toast.error('Session expired - please login again');
+      }
       break;
     case 404:
       history.push('/not-found');
@@ -93,7 +96,8 @@ const Account = {
   current: () => request.get<User>('/account'),
   login: (user: UserFormValues) => request.post<User>('/account/login', user),
   register: (user: UserFormValues) => request.post<User>('/account/register', user),
-  fbLogin: (accessToken: string) => request.post<User>(`/account/fbLogin?accessToken=${accessToken}`, {})
+  fbLogin: (accessToken: string) => request.post<User>(`/account/fbLogin?accessToken=${accessToken}`, {}),
+  refreshToken: () => request.post<User>('/account/refreshToken', {})
 }
 
 const Profiles = {
