54469bb13e0facd67f04626be31a65a7ae8ebc5d
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-05 21:01:18 -0700

    Add resendEmailConfirmationLink controller, tested in postman

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -131,6 +131,27 @@ namespace API.Controllers
             return Ok("Email confirmed - you can now login");
         }
 
+        [AllowAnonymous]
+        [HttpGet("resendEmailConfirmationLink")]
+        public async Task<IActionResult> ResendEmailConfirmationLink(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null) return Unauthorized();
+
+            var origin = Request.Headers["origin"];
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+            var verifyUrl = $"{origin}/account/verifyEmail?token={token}&email={user.Email}";
+
+            var message = $"<p>Please click the below link to verify your email address:</p><p><a href='{verifyUrl}'>Click to verify email</a></p>";
+
+            await _emailSender.SendEmailAsync(user.Email, "Please verify email", message);
+
+            return Ok("Email verification link re-sent");
+        }
+
         [Authorize]
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
